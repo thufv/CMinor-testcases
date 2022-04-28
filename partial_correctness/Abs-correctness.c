682f8a686f4ee4a8dd08ea0131fef4cd34637b7c
Zhilei Han <linusboyle@outlook.com>
2022-04-28 21:39:47 +0800

    correct Abs

--- partial_correctness/Abs-correctness.c
+++ partial_correctness/Abs-correctness.c
@@ -1,19 +1,20 @@
 /*@
- * requires \valid(a_0+(0..n-1));
- * requires n >= 1;
- * ensures \valid(\result+(0..n-1));
- * ensures \forall integer ix; 0 <= ix < n ==> \result[ix] >= 0;
+  requires \valid(a_0+(0..n-1));
+  requires n >= 1;
  */
-int[] abs(int[] a_0, int n) {
+void abs(int a_0[], int n) {
     /*@
-     * loop invariant 0 <= i <= n && \forall integer ix; 0 <= ix < i ==> a_0[ix] >= 0;
+      loop invariant 0 <= i <= n && \forall integer ix; 0 <= ix < i ==> a_0[ix] >= 0;
+      loop invariant \valid(a_0+(0..n-1));
      */
 	for
-		(int i = 0; i < n; i = i + 1)
+		(int i = 0; i < n; i = i + 1) 
 	{
 		if (a_0[i] < 0) {
 			a_0[i] = -a_0[i];
 		}
 	}
-	return a_0;
+   //@ assert \valid(a_0+(0..n-1));
+   //@ assert \forall integer ix; 0 <= ix < n ==> a_0[ix] >= 0;
+	return;
 }
