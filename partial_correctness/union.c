027eb49a359565a9bcc8a189dd12f1a637ce4fc9
namasikanam <namasikanam@gmail.com>
2022-05-01 16:03:12 +0800

    fix union.c

--- partial_correctness/union.c
+++ partial_correctness/union.c
@@ -1,5 +1,4 @@
 /*@
-<<<<<<< HEAD
   predicate inRange(integer target, integer[] arr, integer n, integer low, integer high) =
     ((low <= high && 0 <= low && high <= n - 1) ==>
     (\exists integer ix; (low <= ix && ix <= high && arr[ix] == target)));
@@ -22,10 +21,11 @@ void unio(int a[], int n, int b[], int m, int u[]) {
     int j = 0;
 
     /*@ 
-      loop invariant n >= 0 && m >= 0 && j >= 0 &&
-            (i > 0 && j > 0 && a[i-1] == u[j-1]
-            ==> (\exists int ix; (0 <= ix && ix <= i - 1 && a[ix] == 1)
-                <==> \exists int ix; (0 <= ix && ix <= j - 1 && u[ix] == 1)));
+      loop invariant n > 0 && m > 0;
+      loop invariant j >= 0;
+      loop invariant i > 0 && j > 0 && a[i - 1] == u[j - 1]
+                      ==> (\exists integer ix; (0 <= ix <= i - 1 && a[ix] == 1)
+                            <==> \exists integer ix; (0 <= ix <= j - 1 && u[ix] == 1));
     */
     for (int i = 0; i < n; i = i + 1)
     {
@@ -33,13 +33,12 @@ void unio(int a[], int n, int b[], int m, int u[]) {
             j = j + 1;
     }
     /*@
-      loop invariant n >= 0 && m >= 0 && (in(1,a, n) || in(1,b, m) <==> in(1,u, n+m));
-    */
+        loop invariant n > 0 && m > 0;
+        loop invariant (in(1,a, n) || in(1,b, m) <==> in(1,u, n+m));
+     */
     for (int i = 0; i < m; i = i + 1)
     {
             u[j] = b[i];
             j = j + 1;
     }
-
-    return u;
 }
